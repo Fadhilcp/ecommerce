7989e332d4c9093b25753197444f0bdd8d778713
Fadhilcp <fadhilrahamancph@gmail.com>
2026-06-10 21:26:53 +0530

    google callback url into env

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ const env = require('dotenv').config()
 passport.use(new googleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:'https://perfumora.website/auth/google/callback'
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
 },
 
 async (accessToken,refreshToken,profile,done)=>{
